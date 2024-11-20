e7b7a32b6d4518578e1f1c9c438a9eb875d4ab10
Mateus Acauhi <mateusacauhi@bluems.com>
2024-11-20 14:27:01 -0300

    add: estrutura de rotas e permisoes

--- ProjectManagerFront/src/App.js
+++ ProjectManagerFront/src/App.js
@@ -1,10 +1,63 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import LoginForm from "./components/LoginForm";
+import ProjectList from "./components/ProjectList";
+import TaskList from "./components/TaskList";
+import AdminPanel from "./components/AdminPanel";
+import HomePage from "./components/HomePage";
+import RegisterForm from "./components/RegisterForm";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { jwtDecode } from "jwt-decode";
+
+function PrivateRoute({ children }) {
+  const token = localStorage.getItem("token");
+  const isAuthenticated = token && jwtDecode(token).exp > Date.now() / 1000;
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
+}
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!!!</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <ToastContainer />
+        <Routes>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/projects"
+            element={
+              <PrivateRoute>
+                <ProjectList />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/tasks"
+            element={
+              <PrivateRoute>
+                <TaskList />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <PrivateRoute>
+                <AdminPanel />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
