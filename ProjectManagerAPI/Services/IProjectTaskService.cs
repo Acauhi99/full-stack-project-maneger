4f20c38802862f9e98530289020dae528a1a53b1
Mateus Acauhi <mateusacauhi@bluems.com>
2024-11-23 00:34:59 -0300

    add: interface de comunicacao com service de tarefa

--- ProjectManagerAPI/Services/IProjectTaskService.cs
+++ ProjectManagerAPI/Services/IProjectTaskService.cs
@@ -1,8 +1,12 @@
-using System;
-
 namespace ProjectManagerAPI.Services;
+
+using ProjectManagerAPI.DTOs;
 
 public interface IProjectTaskService
 {
-
+    Task<IEnumerable<ProjectTaskDTO>> GetAllTasksAsync();
+    Task<ProjectTaskDTO?> GetTaskByIdAsync(Guid id);
+    Task<ProjectTaskDTO> CreateTaskAsync(ProjectTaskDTO taskDto);
+    Task<ProjectTaskDTO?> UpdateTaskAsync(Guid id, ProjectTaskDTO taskDto);
+    Task<bool> DeleteTaskAsync(Guid id);
 }
