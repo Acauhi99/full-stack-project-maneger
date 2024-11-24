6faea229c127f0fa19305e6e287db9bf31039714
FernandaRooke <fernanda.r.silva@viannasempre.com.br>
2024-11-24 12:36:43 -0300

    refactor: alterado url base

--- ProjectManagerFront/src/services/api.js
+++ ProjectManagerFront/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:7009/",
+  baseURL: "http://localhost:7009/",
 });
 
 api.interceptors.request.use((config) => {
