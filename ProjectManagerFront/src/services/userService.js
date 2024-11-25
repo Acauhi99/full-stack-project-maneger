9f02d015015fe0d0dc27c309f181897cdbeeb2d1
Lucas Menon <lucasguilarduccimenon@gmail.com>
2024-11-25 16:30:17 -0300

    fix: redirecionamento de login

--- ProjectManagerFront/src/services/userService.js
+++ ProjectManagerFront/src/services/userService.js
@@ -1,10 +1,19 @@
 import api from "./api";
 import { handleApiError } from "./apiErrors";
 
+export const parseJwt = (token) => {
+  try {
+    return JSON.parse(atob(token.split('.')[1]));
+  } catch (e) {
+    return null;
+  }
+};
 export const userService = {
+
   async login(email, senha) {
     try {
       const response = await api.post("/users/login", { email, senha });
+      console.log(response.data.token);
       localStorage.setItem("token", response.data.token);
       return response.data;
     } catch (error) {
